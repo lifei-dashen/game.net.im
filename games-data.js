4b696fee5f204a6826bb1444cc5080906d3251d0
lifei-dashen <lifei444208421@gmail.com>
2025-06-14 00:03:53 +0800

    Create games-data.js

---
+++ games-data.js
@@ -0,0 +1,103 @@
+const GAMES_DATA = {
+    trending: [
+        {
+            id: "urf-001",
+            title: "Ultimate Robot Fighting",
+            category: "Action",
+            tags: ["fighting", "robots", "multiplayer"],
+            thumb: "https://img.gamemonetize.com/urf.jpg",
+            url: "https://games.gamemonetize.com/ultimate-robot-fighting/",
+            plays: 250000,
+            rating: 4.8
+        },
+        {
+            id: "csl-002",
+            title: "Construction Simulator",
+            category: "Simulation",
+            tags: ["building", "vehicles", "management"],
+            thumb: "https://img.gamemonetize.com/csl.jpg",
+            url: "https://games.gamemonetize.com/construction-simulator-lite/",
+            plays: 180000,
+            rating: 4.5
+        },
+        // ... 继续添加48个trending游戏
+    ],
+    casual: [
+        {
+            id: "bb-001",
+            title: "Balloon Blitz",
+            category: "Casual",
+            tags: ["arcade", "colorful", "family"],
+            thumb: "https://img.gamemonetize.com/bb.jpg",
+            url: "https://games.gamemonetize.com/balloon-blitz/",
+            plays: 85000,
+            rating: 4.3
+        },
+        // ... 继续添加99个casual游戏
+    ],
+    puzzle: [
+        {
+            id: "sm-001",
+            title: "SokoMatch",
+            category: "Puzzle",
+            tags: ["matching", "brain", "strategy"],
+            thumb: "https://img.gamemonetize.com/sm.jpg",
+            url: "https://games.gamemonetize.com/sokomatch/",
+            plays: 95000,
+            rating: 4.6
+        },
+        // ... 继续添加79个puzzle游戏
+    ],
+    action: [
+        {
+            id: "sgo-001",
+            title: "Stickman Ghost Online",
+            category: "Action",
+            tags: ["fighting", "multiplayer", "adventure"],
+            thumb: "https://img.gamemonetize.com/sgo.jpg",
+            url: "https://games.gamemonetize.com/stickman-ghost-online/",
+            plays: 180000,
+            rating: 4.7
+        },
+        // ... 继续添加99个action游戏
+    ],
+    sports: [
+        {
+            id: "gb-001",
+            title: "Golf Battle",
+            category: "Sports",
+            tags: ["golf", "multiplayer", "competitive"],
+            thumb: "https://img.gamemonetize.com/gb.jpg",
+            url: "https://games.gamemonetize.com/golf-battle/",
+            plays: 120000,
+            rating: 4.4
+        },
+        // ... 继续添加69个sports游戏
+    ],
+    racing: [
+        {
+            id: "sr-001",
+            title: "Speed Racing GT",
+            category: "Racing",
+            tags: ["cars", "speed", "competition"],
+            thumb: "https://img.gamemonetize.com/sr.jpg",
+            url: "https://games.gamemonetize.com/speed-racing-gt/",
+            plays: 150000,
+            rating: 4.5
+        },
+        // ... 继续添加49个racing游戏
+    ],
+    strategy: [
+        {
+            id: "td-001",
+            title: "Tower Defense Master",
+            category: "Strategy",
+            tags: ["tower-defense", "planning", "waves"],
+            thumb: "https://img.gamemonetize.com/td.jpg",
+            url: "https://games.gamemonetize.com/tower-defense-master/",
+            plays: 130000,
+            rating: 4.6
+        },
+        // ... 继续添加49个strategy游戏
+    ]
+}; 
